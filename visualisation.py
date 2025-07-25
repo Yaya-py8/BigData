c18a9d36d5e386ab0995c86110b3f6213b146532
jitendrasah123 <sahjitu74@gmail.com>
2025-07-25 23:07:30 +0100

    added branch

--- visualisation.py
+++ visualisation.py
@@ -1,13 +1,6 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
- 
- #Remember to run git lfs pull to get the dataset since they're large and can't be kept in github.
-
-artists_df = pd.read_csv('Data/artists.csv')
-tracks_df = pd.read_csv('Data/tracks.csv')
-
-print(artists_df.head())
-print(tracks_df.head())
-
+plt.figure(figsize=(10, 6))
+sns.histplot(tracks_df['popularity'], bins=30, kde=True)
+plt.title("Distribution of Track Popularity")
+plt.xlabel("Popularity")
+plt.ylabel("Frequency")
+plt.show()
