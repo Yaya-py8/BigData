56b03c1e683a8280190b3d88a4b49652ffd36f84
Yahye <yahye2.hassan@live.uwe.ac.uk>
2025-07-28 22:09:31 +0100

    updated

--- visualisation.py
+++ visualisation.py
@@ -25,7 +25,7 @@ if duplicated_rows == 0:
 else:
     print(f"\n⚠️ Found {duplicated_rows} duplicated rows in 'tracks_df'. Dropping duplicates...")
     tracks_df = tracks_df.drop_duplicates()
-    print(f"✅ Duplicates removed. Remaining rows: {tracks_df.shape[0]}")
+    print(f"Duplicates removed. Remaining rows: {tracks_df.shape[0]}")
 
 audio_features = ['danceability', 'energy', 'loudness', 'speechiness', 
                   'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
@@ -35,3 +35,18 @@ sns.heatmap(tracks_df[audio_features].corr(), annot=True, cmap='coolwarm')
 plt.title('Correlation Between Audio Features')
 plt.show()
 
+plt.figure(figsize=(10, 7))
+plt.hexbin(tracks_df['danceability'], tracks_df['popularity'], gridsize=50, cmap='viridis', mincnt=1)
+plt.colorbar(label='Number of Tracks')
+plt.xlabel('Danceability')
+plt.ylabel('Popularity')
+plt.title('Density of Tracks by Danceability and Popularity')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.histplot(tracks_df['loudness'], kde=True, bins=50)
+plt.title('Distribution of Track Loudness (dB)')
+plt.xlabel('Loudness (dB)')
+plt.ylabel('Number of Tracks')
+plt.grid(axis='y', alpha=0.75)
+plt.show()
